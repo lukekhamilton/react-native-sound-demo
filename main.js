d45ab8c5bd60e923836b47d944aacc9ecb6d011b
Luke Hamilton <luke@mettadata.com>
2016-11-27 11:41:28 +0800

    Updated with Play, stop and pause button to get a feel for the react-native-sound package

--- main.js
+++ main.js
@@ -7,30 +7,14 @@ import React, {
   TouchableOpacity,
   View
 } from 'react-native';
-var Sound = require('react-native-sound');
 
-class MainView extends Component {
-  render() {
-    return <View style={styles.container}>
-             <TouchableOpacity onPress={this.playSound}>
-               <Text style={styles.button}>play</Text>
-             </TouchableOpacity>
-           </View>;
-  }
+// This doesnt work for some reason
+// import { Sound } from 'react-native-sound';
 
-  playSound() {
-    var s = new Sound('advertising.mp3', Sound.MAIN_BUNDLE, (e) => {
-      if (e) {
-        console.log('error', e);
-      } else {
-        console.log('duration', s.getDuration());
-        s.play();
-      }
-    });
-  }
-}
+const Sound = require('react-native-sound');
 
-var styles = StyleSheet.create({
+
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
@@ -39,9 +23,57 @@ var styles = StyleSheet.create({
   },
   button: {
     fontSize: 20,
-    backgroundColor: 'silver',
+    // backgroundColor: '#c0c0c0',
     padding: 5,
   },
 });
+
+function handleError(error) {
+  if (error) {
+    console.log('failed to load the sound', error);
+  } else { // loaded successfully
+    console.log('duration in seconds: ' + soundFile.getDuration() +
+        'number of channels: ' + soundFile.getNumberOfChannels());
+  }
+}
+
+
+//Load Sound file
+const soundFile = new Sound('advertising.mp3', Sound.MAIN_BUNDLE, handleError);
+
+class MainView extends Component {
+
+  render() {
+    return(
+      <View style={styles.container}>
+
+               <TouchableOpacity onPress={this.handlePlay}>
+                 <Text style={styles.button}>Play</Text>
+               </TouchableOpacity>
+
+               <TouchableOpacity onPress={this.handleStop}>
+                 <Text style={styles.button}>Stop</Text>
+               </TouchableOpacity>
+
+               <TouchableOpacity onPress={this.handlePause}>
+                 <Text style={styles.button}>Pause</Text>
+               </TouchableOpacity>
+
+             </View>
+    )
+  }
+
+  handlePlay(){
+      soundFile.play();
+  }
+
+  handleStop(){
+    soundFile.stop();
+  }
+
+  handlePause(){
+    soundFile.pause();
+  }
+}
 
 export default MainView;
